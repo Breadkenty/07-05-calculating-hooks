6cdc338752f1cf6390ba8dd9bb8faa622f0ac818
Kento Kawakami <kawakamikento@gmail.com>
2020-06-30 22:52:56 -0400

    HTML and CSS styling import

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,32 @@ import NotFound from './pages/NotFound'
 function App() {
   return (
     <Router>
-      <header>
-        <h1>Welcome to my SPA</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Go Home</Link>
-            </li>
-            <li>
-              <Link to="/1">Page 1</Link>
-            </li>
-            <li>
-              <Link to="/2">Page 2</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/1" component={Page}></Route>
-        <Route exact path="/2" component={Page2}></Route>
-        <Route path="*" component={NotFound}></Route>
-      </Switch>
+      <main>
+        <div class="calculator">
+          <div class="display">0</div>
+          <div class="buttons">
+            <button class="button fn">AC</button>
+            <button class="button fn">&#177;</button>
+            <button class="button fn">&#37;</button>
+            <button class="button op">&#247;</button>
+            <button class="button">7</button>
+            <button class="button">8</button>
+            <button class="button">9</button>
+            <button class="button op">&#215;</button>
+            <button class="button">4</button>
+            <button class="button">5</button>
+            <button class="button">6</button>
+            <button class="button op">&#8722;</button>
+            <button class="button">1</button>
+            <button class="button">2</button>
+            <button class="button">3</button>
+            <button class="button op">&#43;</button>
+            <button class="button x2">0</button>
+            <button class="button">.</button>
+            <button class="button op">&#61;</button>
+          </div>
+        </div>
+      </main>
     </Router>
   )
 }
